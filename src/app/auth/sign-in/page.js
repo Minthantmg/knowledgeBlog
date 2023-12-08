2e05e40781cce6b9698b49a4c1e886e0ff4ef64d
Min Thant Mg <minthantmg@gicjp.com>
2023-12-08 16:08:48 +0630

    blog initail commit

---
+++ src/app/auth/sign-in/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <div>
+          Sign In Page
+        </div>
+    );
+};
+
+export default Page;
---
+++ src/app/auth/sign-up/page.js
@@ -0,0 +1,99 @@
+'use client'
+import React, {useState} from 'react';
+import {axiosInstance} from "../../../../util/axiosInstance";
+import {useMutation} from "@tanstack/react-query";
+
+const Page = () => {
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState(false)
+
+    const createUser = async ({name, email, password}) => {
+        try {
+            const res = await axiosInstance.post('/users', {name, email, password})
+            return res.data
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    const {
+        mutateAsync: createUserAsync
+    } = useMutation({
+        mutationKey: ['post', 'users'],
+        mutationFn: createUser,
+    })
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        await createUserAsync({name: username, email, password})
+        //router push to login
+        setUsername('')
+        setEmail('')
+        setPassword('')
+    }
+    return (
+        <div className="container mx-auto px-4 py-8">
+            <div className="max-w-md mx-auto mt-6">
+                <form method="POST" className="max-w-sm mx-auto" onSubmit={handleSubmit}>
+                    <div className="mb-5">
+                        <label
+                            htmlFor="name"
+                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                        >
+                            Name
+                        </label>
+                        <input
+                            type="text"
+                            id="name"
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="John"
+                            onChange={e => setUsername(e.target.value)}
+                            required=""
+                        />
+                    </div>
+                    <div className="mb-5">
+                        <label
+                            htmlFor="email"
+                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                        >
+                            Your email
+                        </label>
+                        <input
+                            type="email"
+                            id="email"
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            placeholder="name@flowbite.com"
+                            required=""
+                            onChange={e => setEmail(e.target.value)}
+                        />
+                    </div>
+                    <div className="mb-5">
+                        <label
+                            htmlFor="password"
+                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                        >
+                            Your password
+                        </label>
+                        <input
+                            type="password"
+                            id="password"
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            required=""
+                            onChange={e => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <button
+                        type="submit"
+                        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    >
+                        Submit
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default Page;
